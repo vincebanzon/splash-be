4ef3b2bf8d52bdf718b74f1df4cf970dad30d0b1
Vince Banzon <vincebanzon@gmail.com>
2024-08-16 16:34:04 +0400

    Create Chat Message functionality

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,46 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  MessageBody,
+  OnGatewayDisconnect,
+  OnGatewayConnection,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+import { AddMessageDto } from './dto/add-message.dto';
+
+@WebSocketGateway({ cors: { origin: '*' } })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  private logger = new Logger('ChatGateway');
+
+  @SubscribeMessage('chat') // subscribe to chat event messages
+  handleMessage(@MessageBody() payload: AddMessageDto): AddMessageDto {
+    this.logger.log(`Message received: ${payload.author} - ${payload.body}`);
+    this.server.emit('chat', payload); // broadbast a message to all clients
+    return payload; // return the same payload data
+  }
+
+  // it will be handled when a client connects to the server
+  handleConnection(socket: Socket) {
+    this.logger.log(`Socket connected: ${socket.id}`);
+    // broadbast a message to all clients
+    this.server.emit('chat', {
+      author: 'bot',
+      body: 'Someone joined the room.'
+    });
+  }
+
+  // it will be handled when a client disconnects from the server
+  handleDisconnect(socket: Socket) {
+    this.logger.log(`Socket disconnected: ${socket.id}`);
+    this.server.emit('chat', {
+      author: 'bot',
+      body: 'Someone left the room.'
+    });
   }
 }
